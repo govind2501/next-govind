1eab3856c0e059eb0ea9516306e42b845376a95f
Govind kumar verma <govindkumar1389@gmail.com>
2026-09-07 16:45:51 +0530

    login page
    Signed-off-by: Govind kumar verma <govindkumar1389@gmail.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -99,7 +99,7 @@ export default function Navbar() {
 
             {/* Login and Signup - always visible */}
             <Link href="/login" className='hover:text-orange-300'>
-              Login
+              Login Page
             </Link>
             <Link
               href="/signup"
